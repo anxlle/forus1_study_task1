1383b969223f874cba760a797f77c59f1dbd6b55
ANX <danielgrischaeff@yandex.ru>
2025-06-24 14:42:25 +0800

    Fixed index.js to main_router, added error json

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.use("/static", function(_, res) {
 
 app.use("/dynamic", function(_, res) {
 	const a = _.query.a, b = _.query.b, c = _.query.c;
+	const error = {
+		header: 'Error'
+	};
+	if (isNaN(a) || isNaN(b) || isNaN(c) || !a || !b || !c)
+		return res.json(error);
 	const data = {
 		header: 'Calculated',
 		body: a*b*c/3
