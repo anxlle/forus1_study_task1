40dcba21475157d43f049c7dcb4c86a168077b33
ANX <danielgrischaeff@yandex.ru>
2025-06-27 15:24:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const tgbot = require('node-telegram-bot-api');
 const token = '7533819906:AAEYRnXI062GSQnqHipGuUdGQdh2p_rFv6s';
 const bot = new tgbot(token, {polling: true});
 
-bot.onText(/\/start/, (msg) => {
-	bot.sendMessage(msg.chat.id, "Привет, октагон!");
-});
-
 bot.on('message', (msg) => {
-	var help = "/help", site = "/site", creator = "/creator";
+	var start = "/start", help = "/help", site = "/site", creator = "/creator";
+
+	if (msg.text.toString().toLowerCase().indexOf(start) === 0)
+		bot.sendMessage(msg.chat.id, "Привет, октагон!");
 	
 	if (msg.text.toString().toLowerCase().indexOf(help) === 0)
 		bot.sendMessage(msg.chat.id, "Список команд:\n/site - отправляет в чат ссылку на сайт октагона\n/creator - отправляет в чат ФИО");
