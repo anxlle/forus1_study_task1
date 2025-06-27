6fb1384eb8c35c30515fd8e5c5a9ec3a0daab07c
ANX <danielgrischaeff@yandex.ru>
2025-06-27 15:10:45 +0800

    Commit to main_telegram_commands, added commands

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const bot = new tgbot(token, {polling: true});
 bot.onText(/\/start/, (msg) => {
 	bot.sendMessage(msg.chat.id, "Привет, октагон!");
 });
+
+bot.on('message', (msg) => {
+	var help = "/help", site = "/site", creator = "/creator";
+	
+	if (msg.text.toString().toLowerCase().indexOf(help) === 0)
+		bot.sendMessage(msg.chat.id, "Список команд:\n/site - отправляет в чат ссылку на сайт октагона\n/creator - отправляет в чат ФИО");
+		
+	if (msg.text.toString().toLowerCase().indexOf(site) === 0)
+		bot.sendMessage(msg.chat.id, "https://octagon-students.ru/");
+
+	if (msg.text.toString().toLowerCase().indexOf(creator) === 0)
+		bot.sendMessage(msg.chat.id, "Гришаев Даниил Геннадьевич");
+});
